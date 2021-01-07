1b23d24c8be8c140c21b8d82f9a90106ac73ffcb
tao <19983541949@163.com>
2021-01-07 16:35:18 +0800

    配置显示sql

--- src/main/java/com/ymkj/analysis/controller/BaseNetController.java
+++ src/main/java/com/ymkj/analysis/controller/BaseNetController.java
@@ -1,12 +1,14 @@
 package com.ymkj.analysis.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.ymkj.analysis.domain.BaseNetPrice;
 import com.ymkj.analysis.service.BaseNetPriceService;
 import com.ymkj.analysis.service.query.NetPriceQuery;
 import com.ymkj.analysis.service.vo.NetPriceVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author tao
  * @Date 2021/1/7
  **/
+@Api(tags = "网价API")
 @RestController
+@RequestMapping(value = "/net-price")
 public class BaseNetController {
     @Autowired
     private BaseNetPriceService baseNetPriceService;
+
     @GetMapping
-    public Page<NetPriceVO> getPage(NetPriceQuery query){
+    @ApiOperation(value = "条件分页查询网价")
+    public Page<NetPriceVO> getPage(NetPriceQuery query) {
         return baseNetPriceService.listWithPage(query);
     }
+
 }
